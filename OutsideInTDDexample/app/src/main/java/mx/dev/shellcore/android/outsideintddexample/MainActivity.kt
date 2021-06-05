543ce97096e1b005f1462d64c676a103e5e17534
Cesar Morales Garduño <shell.one.core@gmail.com>
2021-06-05 18:04:38 -0500

    Creación de proyecto OutsideIn TDD

---
+++ OutsideInTDDexample/app/src/main/java/mx/dev/shellcore/android/outsideintddexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.dev.shellcore.android.outsideintddexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
