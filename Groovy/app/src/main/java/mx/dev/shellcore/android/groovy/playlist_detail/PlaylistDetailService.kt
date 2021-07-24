23f06c08702f8628bccac7038eb4602a6b9e57cc
Cesar Morales Garduño <shell.one.core@gmail.com>
2021-07-24 12:44:56 -0500

    Implementación de pruebas para servicio y vista de detalle de playlist

--- Groovy/app/src/main/java/mx/dev/shellcore/android/groovy/playlist_detail/PlaylistDetailService.kt
+++ Groovy/app/src/main/java/mx/dev/shellcore/android/groovy/playlist_detail/PlaylistDetailService.kt
@@ -1,10 +1,19 @@
 package mx.dev.shellcore.android.groovy.playlist_detail
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import mx.dev.shellcore.android.groovy.playlist.PlaylistApi
+import java.lang.RuntimeException
 import javax.inject.Inject
 
-class PlaylistDetailService @Inject constructor() {
+class PlaylistDetailService @Inject constructor(private val api: PlaylistApi) {
+
     suspend fun fetchPlaylistDetail(id: String) : Flow<Result<PlaylistDetail>> {
-        TODO("Not yet implemented")
+        return flow {
+            emit(Result.success(api.fetchPlaylistById(id)))
+        }.catch {
+            emit(Result.failure(RuntimeException("Something went wrong")))
+        }
     }
 }
